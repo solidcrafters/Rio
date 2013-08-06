15cd3b295ab34ccc4eb9242ad98854a60fdd6431
Antoine <antoine@crafting-labs.fr>
2013-08-06 22:39:23 +0200

    move grunt related devDependencies to dependencies
    grunt is use by heroku to build the app before serving it and heroku only 'npm install --production'

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,7 +14,7 @@ var mountFolder = function (connect, dir) {
 
 module.exports = function (grunt) {
   // load all grunt tasks
-  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+  require('matchdep').filter('grunt-*').forEach(grunt.loadNpmTasks);
 
   // configurable paths
   var yeomanConfig = {
