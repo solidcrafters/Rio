84151b520d62f27a5e3a1ee1d994f4b6f8b96c82
jerome-genba <jerome@avoustin.com>
2013-08-12 11:02:23 +0200

    Use of PhantomJS in client tests

--- karma.conf.js
+++ karma.conf.js
@@ -49,7 +49,7 @@ autoWatch = false;
 // - Safari (only Mac)
 // - PhantomJS
 // - IE (only Windows)
-browsers = ['Firefox'];
+browsers = ['Firefox', 'PhantomJS'];
 
 // If browser does not capture in given timeout [ms], kill it
 captureTimeout = 5000;
